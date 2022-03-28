749feec6721f46f1e41a7ec7ec455542885bc2b2
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-03-28 16:55:47 +0200

    Day 1 with two parts

--- advent1-1/src/main.rs
+++ advent1-1/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string("data.txt").expect("Failed to read file");
+    let mut result = 0;
+    let mut previous_number: i32 = 2000000000; 
+
+    for line in data.lines() {
+
+        let number = line.parse::<i32>().expect("Failed to parse number");
+
+        if previous_number < number {
+            result += 1;
+        }
+
+        previous_number = number;
+    }
+
+    println!("{}", result);
 }
--- advent1-2/src/main.rs
+++ advent1-2/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string("../data.txt").expect("Failed to read file");
+
+    let numbers: Vec<i32> = data.lines()
+                                .map(|number| number.parse::<i32>().expect("Failed to parse number"))
+                                .collect();
+
+    let mut result: u32 = 0;
+    let mut index: usize = 0;
+    let mut previous_number: i32 = 2000000000; 
+
+    while index < numbers.len() - 2 {
+
+        let slice = &numbers[index..index + 3];
+
+        let sum: i32 = slice.iter().sum();
+
+        if previous_number < sum {
+            result += 1;
+        }
+        index += 1;
+        previous_number = sum;
+    }
+
+    println!("{}", result);
 }
