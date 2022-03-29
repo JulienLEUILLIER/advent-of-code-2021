80da768623f9d581361cb694723d49919afd4c20
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-03-29 18:34:09 +0200

    Used enums to better handle different directions

--- advent2-1/src/main.rs
+++ advent2-1/src/main.rs
@@ -1,42 +1,67 @@
 use std::fs;
 
 fn main() {
-    let data = fs::read_to_string("data.txt").expect("Failed to read the file");
+    enum Direction {
+        Forward,
+        Up,
+        Down,
+        None
+    }
+
+    struct Controls {
+        direction: Direction,
+        amount: i32,
+    }
 
     #[derive(Debug)]
-    struct Position {
+    struct Submarine {
         horizontal: i32,
         vertical: i32,
     }
 
-    impl Position {
+    
+    impl Submarine {
         fn set_depth(&mut self, amount: i32) -> () {
             self.vertical += amount;
         }
-
+        
         fn set_horizontal(&mut self, amount: i32) -> () {
             self.horizontal += amount;
         }
     }
-
-    let mut submarine = Position {
+    
+    let mut submarine = Submarine {
         horizontal: 0,
-        vertical: 0,
+        vertical: 0
     };
 
-    for line in data.lines() {
-        let words: Vec<&str> = line.split(' ').collect();
+    let data = fs::read_to_string("data.txt").expect("Failed to read text file");
 
-        let amount = words[1].parse::<i32>().expect("Failed to parse number");
+    let controls: Vec<Controls> = data
+        .lines()
+        .map(|line| {
+            let line_values: Vec<&str> = line.split(' ').collect();
+            let direction = match line_values[0] {
+                "forward" => Direction::Forward,
+                "up" => Direction::Up,
+                "down" => Direction::Down,
+                _ => Direction::None
+            };
 
-        match words[0] {
-            "forward" => submarine.set_horizontal(amount),
-            "up" => submarine.set_depth(-amount),
-            "down" => submarine.set_depth(amount),
-            _ => ()
+            let amount = line_values[1].parse::<i32>().expect("Failed to parse value");
+
+            Controls { direction, amount }
+        }).collect();
+
+    for control in controls {
+        match control.direction {
+            Direction::Forward => submarine.set_horizontal(control.amount),
+            Direction::Up => submarine.set_depth(-control.amount),
+            Direction::Down => submarine.set_depth(control.amount),
+            Direction::None => println!("Error : Unknown command"),
         }
     }
-
+    
     println!("{:#?}", submarine);
 
     println!("{}", submarine.horizontal * submarine.vertical);
